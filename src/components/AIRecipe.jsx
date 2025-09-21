d808716d17e9f112f8571714ce4ad94a8aef4252
kuldeep <kuldeeps48@gmail.com>
2025-09-21 22:07:12 +0530

    Added smooth scroll using useEffect, useRef

--- src/components/AIRecipe.jsx
+++ src/components/AIRecipe.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Markdown from "react-markdown";
 import { fetchRecipe, FAILED_MSG } from "../services/ai";
 
 export default function AIRecipe(props) {
   const [recipe, setRecipe] = useState("");
+  const getRecipeSection = useRef(null);
+
+  useEffect(() => {
+    if (recipe?.length > 0 && getRecipeSection.current) {
+      getRecipeSection.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [recipe]);
 
   async function generateRecipe() {
     const r = await fetchRecipe(props.ingredients);
@@ -11,7 +20,7 @@ export default function AIRecipe(props) {
   }
 
   return (
-    <div>
+    <div ref={getRecipeSection}>
       <section className="get-recipe-container">
         <h2>Ready to generate the recipe?</h2>
         <button
